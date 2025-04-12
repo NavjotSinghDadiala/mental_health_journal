33c543197084fe04f53455a1f5e9c33e50fed310
rrttc <rrttcofficial@gmail.com>
2025-04-12 07:19:37 +0530

    new change

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+# Install system dependencies
+apt-get update
+apt-get install -y portaudio19-dev
+
+# Install Python dependencies
+pip install -r requirements.txt
